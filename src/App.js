56785118c0582e3052ddd9129cf2633f6f030fae
anjali-chanda-70565 <anjalichanda2019@gmail.com>
2023-05-04 11:53:57 +0530

    change css to sass and merge all css files

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import "bootstrap/dist/css/bootstrap.min.css";
-import "./App.css";
 import Header from "./components/Header";
 // import Hero from "./components/Hero";
-import "./master.css";
 import SecondSection from "./components/SecondSection";
 import ThirdSection from "./components/ThirdSection";
 import Hero2 from "./components/Hero2";
