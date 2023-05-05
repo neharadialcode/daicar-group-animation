255fbf50338840bbe30326badd3b02d3271d065f
itzkunal01 <kunal.sharmag12@gmail.com>
2023-05-05 10:49:06 +0530

    updating code

--- src/App.js
+++ src/App.js
@@ -29,7 +29,7 @@ function App() {
       <Footer />
       {/* <SeventhSection /> */}
       {/* <EightSection /> */}
-      <ScrollChatIcon />
+      {/* <ScrollChatIcon /> */}
       <BackToTop />
     </>
   );
