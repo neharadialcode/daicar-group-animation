064c46d62914b7fc73b87894ffaea5e2e8032ab6
itzkunal01 <kunal.sharmag12@gmail.com>
2023-05-18 09:48:10 +0530

    updating hero-section code

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -8,6 +8,7 @@ import Header from "./Header";
 
 import ipad from "../assets/ipad.png";
 import ipadWhite from "../assets/ipad-white.png";
+import ipad_mockup from "../assets/images/png/hero_slide_iPad_mockup_02.png";
 import arrow from "../assets/arrow.svg";
 
 const Hero = () => {
@@ -480,6 +481,16 @@ const Hero = () => {
         autoAlpha: 0,
       })
         .to(
+          ".arrow-hero-two",
+          {
+            // scale: 0.2,
+            // rotate: "45deg",
+            yPercent: -200,
+            autoAlpha: 0,
+          },
+          "<"
+        )
+        .to(
           ".arrow-hero-three",
           {
             yPercent: -300,
@@ -491,14 +502,6 @@ const Hero = () => {
           yPercent: 100,
           opacity: 0,
         })
-        .to(
-          ".arrow-hero-two",
-          {
-            scale: 0.2,
-            rotate: "45deg",
-          },
-          "<"
-        )
         .set("#hero-outline-arrow", {
           autoAlpha: 1,
         })
@@ -829,13 +832,13 @@ const Hero = () => {
                     <span className="d-inline-block hero_heading_btn hero_slide_3_btn me-2 mx-sm-2 mx-lg-3">
                       vendite
                     </span>
-                    <span>
+                    <span className="me-2">
                       <img
                         className="hero_e_text"
                         src={heroEtext}
                         alt="hero-e-text"
                       />
-                    </span>{" "}
+                    </span>
                     digitale
                   </h2>
                   <h3 className="font_8xl fw-bold number_heading pt-4 pb-4 text-center color_light_green">
@@ -849,7 +852,7 @@ const Hero = () => {
                   <p className="font_4xl color_white_off ff_mundial_regular text-center pt-5 pt-lg-2 pt-xl-0 pb-4 hero_slide_para_2">
                     Daicar aiuta a far
                     <span className="ff_mundial_bold mx-2">crescere</span> la
-                    tua azienda, <br className="d-none d-sm-block" />
+                    tua azienda, <br className="d-none d-xl-block" />
                     creando contatti e generando vendite
                   </p>
                 </div>
@@ -877,11 +880,11 @@ const Hero = () => {
                     <span className="ff_mundial_bold mx-2">Generate</span>
                     nel 2022
                   </p>
-                  <p className="font_4xl color_white_off ff_mundial_regular text-center pt-5 pt-lg-2 pt-xl-0 pb-4 hero_slide_para_2">
+                  <p className="font_4xl color_white_off ff_mundial_regular text-center pt-5 pt-lg-2 pt-xl-0 pb-4 hero_slide_para_2 campagne_para">
                     Campaign management
                     <span className="ff_mundial_bold mx-2">flessibile</span> e
                     altamente
-                    <br className="d-none d-sm-block" />
+                    <br className="d-none d-xl-block" />
                     <span className="ff_mundial_bold mx-2">
                       personalizzabile
                     </span>
@@ -890,7 +893,6 @@ const Hero = () => {
                   </p>
                 </div>
               </div>
-
               <div
                 id="hero-outline-arrow"
                 className="position-absolute z-10 pointer-events-none"
@@ -910,121 +912,22 @@ const Hero = () => {
                 src={arrow}
               />
             </div>
-            {/* <div id="intro-content" className="position-relative h-100 w-100"> */}
-            {/* <img
-                id="intro-image-4"
-                src={Image4}
-                className="position-absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 h-96 w-[600px] object-cover"
-              />
-              <img
-                id="intro-image-3"
-                src={Image3}
-                className="position-absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 h-96 w-[600px] object-cover"
-              />
-              <img
-                id="intro-image-2"
-                src={Image2}
-                className="position-absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 h-96 w-[600px] object-cover"
-              />
-              <img
-                id="intro-image-1"
-                src={Image1}
-                className="position-absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 h-96 w-[600px] object-cover"
-              /> */}
-            {/* <div
-                id="intro-navigation"
-                className="position-absolute intro-navigation w-100 d-flex justify-content-center align-items-center"
-              > */}
-            {/* =================== */}
-            {/* <div className="text-center pb-sm-5 pb-xxl-0">
-                <div className="max_width_tabs d-flex justify-content-center tabs_wrap pt-sm-5 pt-lg-0">
-                  {buttonData.map((obj, index) => (
-                    <div className="px-2 pt-sm-2" key={index}>
-                      <div className={`card_img_${index}`}>
-                        <div
-                          className={`${
-                            index === 5
-                              ? "d-none"
-                              : `${
-                                  index === 0
-                                    ? "tabs_lottie_1 position-absolute start-0 custom_height_tab_img tabs_img"
-                                    : "position-absolute start-0 w-100 pt-3 custom_height_tab_img tabs_img tabs_lottie"
-                                }`
-                          }`}
-                        >
-                          <Lottie
-                            className="w-tabs_lottie position-absolute custom_position"
-                            animationData={obj.card_img}
-                            loop={true}
-                            play
-                          />
-                        </div>
-                      </div>
-                      <div className="d-flex align-items-center pt-sm-3 pt-lg-0">
-                        <p className="font_2xl color_gray pt-3">{obj.para}</p>
-                        {obj.buttonText &&
-                          (console.log(obj.buttonId, "hello"),
-                          (
-                            <button
-                              className={`active_button_${index} tabs_btn ms-2 ms-sm-3 font_2xl color_gray`}
-                              dangerouslySetInnerHTML={{
-                                __html: obj.buttonText,
-                              }}
-                              onClick={() => moveTo("hero", obj.label)}
-                              id={obj.buttonId}
-                            ></button>
-                          ))}
-                      </div>
-                    </div>
-                  ))}
-                </div>
-              </div> */}
-
-            {/* ======================================================= */}
-            {/* <div
-                  id="intro-image-1-btn"
-                  className="bg-green-200 px-10 py-3 rounded-xl font-bold"
-                  onClick={() => moveTo("hero", "btn-1")}
-                >
-                  Image 1
-                </div>
-                <div
-                  id="intro-image-2-btn"
-                  className="bg-green-200 px-10 py-3 rounded-xl font-bold"
-                  onClick={() => moveTo("hero", "btn-2")}
-                >
-                  Image 2
-                </div>
-                <div
-                  id="intro-image-3-btn"
-                  className="bg-green-200 px-10 py-3 rounded-xl font-bold"
-                  onClick={() => moveTo("hero", "btn-3")}
-                >
-                  Image 3
-                </div>
-                <div
-                  id="intro-image-4-btn"
-                  className="bg-green-200 px-10 py-3 rounded-xl font-bold"
-                  onClick={() => moveTo("hero", "btn-4")}
-                >
-                  Image 4
-                </div> */}
-            {/* ======================================================= */}
-            {/* </div> */}
-            {/* </div> */}
-
             <img
               id="hero-ipad"
               className="position-absolute -z-10 pointer-events-none origin-center ipad_img"
+              src={ipad_mockup}
+            />
+            <img
+              id="hero-ipad"
+              className="position-absolute -z-10 pointer-events-none origin-center ipad_img d-none d-xl-block"
               src={ipad}
             />
             <img
               id="hero-ipad-white"
-              className="position-absolute -z-10 pointer-events-none origin-center ipad_img opacity-0"
+              className="position-absolute -z-10 pointer-events-none origin-center ipad_img opacity-0 d-none d-xl-block"
               src={ipadWhite}
             />
           </div>
-          {/* <div id="white-section" className="bg_dark_gray inset-0 -z-10"></div> */}
         </div>
       </div>
     </>
