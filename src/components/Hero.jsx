72c3060846bc6608a32fd76f48533d598325194d
itzkunal01 <kunal.sharmag12@gmail.com>
2023-05-30 16:30:21 +0530

    updated code

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -760,14 +760,14 @@ const Hero = () => {
           // ["#hero-ipad", ".hero_slide_2"],
           "#hero-ipad",
           {
-            scale: 0.7,
+            scale: 0.8,
           },
           ">"
         )
         .to(
           ".hero_slide_2",
           {
-            scale: 0.5,
+            scale: 0.4,
             top: "30%",
           },
           "<"
@@ -2453,18 +2453,16 @@ const Hero = () => {
               </div>
             </div>
 
-            <div className="ipad_parent">
-              <img
-                id="hero-ipad"
-                className="position-absolute -z-10 pointer-events-none origin-center ipad_img"
-                src={ipad}
-              />
-              <img
-                id="hero-ipad-white"
-                className="position-absolute -z-10 pointer-events-none origin-center ipad_img opacity_0"
-                src={ipadWhite}
-              />
-            </div>
+            <img
+              id="hero-ipad"
+              className="position-absolute -z-10 pointer-events-none origin-center ipad_img"
+              src={ipad}
+            />
+            <img
+              id="hero-ipad-white"
+              className="position-absolute -z-10 pointer-events-none origin-center ipad_img opacity_0"
+              src={ipadWhite}
+            />
           </div>
           <div
             id="hero_section"
