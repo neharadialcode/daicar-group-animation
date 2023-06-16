580ac0082afbbb514f03b7a63c1cb59eb35a0227
itzkunal01 <kunal.sharmag12@gmail.com>
2023-06-16 14:10:19 +0530

    chat-icon show from starting and updating code

--- src/components/ScrollChatIcon.jsx
+++ src/components/ScrollChatIcon.jsx
@@ -2,40 +2,40 @@ import React, { useState, useEffect } from "react";
 import { ChatIcon } from "./Icons";
 
 const ScrollChatIcon = () => {
-  const [showContent, setShowContent] = useState(false);
-  useEffect(() => {
-    const handleScroll = () => {
-      if (window.pageYOffset > 300) {
-        // change 500 to your target value
-        setShowContent(true);
-      } else {
-        setShowContent(false);
-      }
-    };
-    window.addEventListener("scroll", handleScroll);
-    return () => {
-      window.removeEventListener("scroll", handleScroll);
-    };
-  }, []);
+  // const [showContent, setShowContent] = useState(false);
+  // useEffect(() => {
+  //   const handleScroll = () => {
+  //     if (window.pageYOffset > 300) {
+  //       // change 500 to your target value
+  //       setShowContent(true);
+  //     } else {
+  //       setShowContent(false);
+  //     }
+  //   };
+  //   window.addEventListener("scroll", handleScroll);
+  //   return () => {
+  //     window.removeEventListener("scroll", handleScroll);
+  //   };
+  // }, []);
 
   return (
     <>
-      <div>
-        {showContent && (
-          <>
-            <div className="show_chat_icon d-flex justify-content-center align-items-center cursor-pointer">
-              <a
-                className="text-decoration-none chat_icon"
-                href="https://www.instagram.com/alfatauristudio/"
-                target="_blank"
-                rel="noreferrer"
-              >
-                <ChatIcon />
-              </a>
-            </div>
-          </>
-        )}
-      </div>
+      {/* <div> */}
+      {/* {showContent && ( */}
+      <>
+        <div className="show_chat_icon d-flex justify-content-center align-items-center cursor-pointer">
+          <a
+            className="text-decoration-none chat_icon"
+            href="https://www.instagram.com/alfatauristudio/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <ChatIcon />
+          </a>
+        </div>
+      </>
+      {/* )} */}
+      {/* </div> */}
     </>
   );
 };
