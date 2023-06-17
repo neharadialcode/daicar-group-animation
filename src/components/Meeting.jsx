1b029fd3efad445ac5b47b436416997c6cd422a1
anjali-chanda-70565 <anjalichanda2019@gmail.com>
2023-06-17 15:43:12 +0530

    set meeting all btn scroll function upr 1280 screen

--- src/components/Meeting.jsx
+++ src/components/Meeting.jsx
@@ -6,69 +6,133 @@ gsap.registerPlugin(ScrollTrigger);
 
 function Meeting() {
   useEffect(() => {
-    let mm = gsap.matchMedia();
-    mm.add("(min-width: 1200px)", () => {
+    // let mm = gsap.matchMedia();
+    // mm.add("(min-width: 1200px)", () => {
       const tl = gsap.timeline({
         scrollTrigger: {
           trigger: "#contatti",
-          start: "top top",
+          start: "top 70%",
+          end: "bottom bottom",
           scrub:true,
-          pin: true,
           fastScrollEnd: true,
         },
       });
       tl.from([".meeting_first_line"], {
-        y: -100,
+        x: -100,
         autoAlpha: 0,
         stagger: 1,
-        duration: 0.8,
       })
         .from([".meeting_second_line"], {
-          y: -100,
+          x: 100,
           autoAlpha: 0,
           stagger: 1,
-          duration: 0.8,
+
         })
         .from([".meeting_third_line"], {
-          y: -100,
+          x: -100,
           autoAlpha: 0,
           stagger: 1,
-          duration: 0.8,
+
         })
         .from([".meeting_fourth_line"], {
-          y: -100,
+          x: 100,
           autoAlpha: 0,
           stagger: 1,
-          duration: 0.8,
+
         })
         .from([".meeting_fifth_line"], {
-          y: -100,
+          x: -100,
           autoAlpha: 0,
           stagger: 1,
-          duration: 0.8,
+
         });
-    });
-    mm.add("(max-width: 1199px)", () => {
-      const tl = gsap.timeline({
-        scrollTrigger: {
-          trigger: "#contatti",
-          start: "top 30%",
-          end: "bottom top",
-          fastScrollEnd: true,
-        },
-      });
-      tl.from(["#meeting-paragraph"], {
-        y: 100,
-        autoAlpha: 0,
-        stagger: 1,
-        ease: "back(3)",
-        duration: 0.8,
-      });
-    });
+    // });
+    // mm.add("(max-width: 1199px)", () => {
+    //   const tl = gsap.timeline({
+    //     scrollTrigger: {
+    //       trigger: "#contatti",
+    //       start: "top 30%",
+    //       end: "bottom top",
+    //       fastScrollEnd: true,
+    //     },
+    //   });
+    //   tl.from(["#meeting-paragraph"], {
+    //     y: 100,
+    //     autoAlpha: 0,
+    //     stagger: 1,
+    //     ease: "back(3)",
+    //     duration: 0.8,
+    //   });
+    // });
   }, []);
-  const meetingBtn = () => {
-    if (window.innerHeight) {
-      gsap.to(window, { duration: 1, scrollTo: 30000, ease: "linear" });
+  const meetingBtnLine1 = () => {
+    if (window.innerWidth >2000) {
+      gsap.to(window, { duration: 1, scrollTo: 34200, ease: "linear" });
+    }
+    if (window.innerWidth >1600 && window.innerHeight <= 1090) {
+      gsap.to(window, { duration: 1, scrollTo: 25800, ease: "linear" });
+    }
+     if (window.innerWidth <1599) {
+      gsap.to(window, { duration: 0.3, scrollTo: 20100, ease: "linear" });
+    }
+     if (window.innerWidth <1300) {
+      gsap.to(window, { duration: 0.3, scrollTo: 20800, ease: "linear" });
+    }
+  };
+  const meetingBtnLine2 = () => {
+    if (window.innerWidth >2000) {
+      gsap.to(window, { duration: 1, scrollTo: 34350, ease: "linear" });
+    }
+    if (window.innerWidth >1600 && window.innerHeight <= 1090) {
+      gsap.to(window, { duration: 1, scrollTo: 26100, ease: "linear" });
+    }
+    if (window.innerWidth <1599) {
+      gsap.to(window, { duration: 1, scrollTo: 20300, ease: "linear" });
+    }
+    if (window.innerWidth <1300) {
+      gsap.to(window, { duration: 1, scrollTo: 21000, ease: "linear" });
+    }
+  };
+  const meetingBtnLine3 = () => {
+    if (window.innerWidth>2000) {
+      gsap.to(window, { duration: 1, scrollTo: 34550, ease: "linear" });
+    }
+    if (window.innerWidth >1600 && window.innerHeight <= 1090) {
+      gsap.to(window, { duration: 1, scrollTo: 26300, ease: "linear" });
+    }
+    if (window.innerWidth <1599) {
+      gsap.to(window, { duration: 1, scrollTo: 20500, ease: "linear" });
+    }
+    if (window.innerWidth <1300) {
+      gsap.to(window, { duration: 1, scrollTo: 21200, ease: "linear" });
+    }
+  };
+  const meetingBtnLine4 = () => {
+    if (window.innerWidth >2000) {
+      gsap.to(window, { duration: 1, scrollTo: 34750, ease: "linear" });
+    }
+    if (window.innerWidth >1600 && window.innerHeight <= 1090) {
+      gsap.to(window, { duration: 1, scrollTo: 26500, ease: "linear" });
+    }
+    if (window.innerWidth <1599) {
+      gsap.to(window, { duration: 1, scrollTo: 22700, ease: "linear" });
+    }
+    if (window.innerWidth <1300) {
+      gsap.to(window, { duration: 1, scrollTo: 21400, ease: "linear" });
+    }
+  };
+  const meetingBtnLine5 = () => {
+    if (window.innerWidth >2000) {
+      gsap.to(window, { duration: 1, scrollTo: 35000, ease: "linear" });
+    }
+    if (window.innerWidth >1600 && window.innerHeight <= 1090) {
+      gsap.to(window, { duration: 1, scrollTo: 26800, ease: "linear" });
+    }
+    if (window.innerWidth <1599) {
+      gsap.to(window, { duration: 1, scrollTo: 22900, ease: "linear" });
+    }
+    if (window.innerWidth <1300) {
+      gsap.to(window, { duration: 1, scrollTo: 21600, ease: "linear" });
     }
   };
   return (
@@ -80,43 +144,50 @@ function Meeting() {
         <div className="custom_container container">
           <div
             id="meeting-paragraph-container"
-            className="mb-0 font_5xl color_dark_gray custom_letter_spacing meeting_para mx-auto px-2 px-sm-0 text-center d-none d-xl-block meeting_stanza"
+            className="mb-0 font_5xl color_dark_gray custom_letter_spacing meeting_para mx-auto px-2 px-sm-0 text-center d-noned-xl-block meeting_stanza"
           >
-            {/* <div> */}
-            <p className="meeting_first_line">
+            <p className="meeting_first_line mb-0 mb-xl-3">
               Ora hai compreso che il futuro delle tue
-              <a href="#footer" onClick={() => meetingBtn()}>
+              <a href="#line_2"
+                onClick={() => meetingBtnLine1()}
+              >
                 <span className="custom_tab_button_012 cursor-pointer common_button color_dark_gray mx-2 font_2xl mx-sm-3">
                   vendite
                 </span>
               </a>
               è digitale e hai scoperto
             </p>
-            <p className="meeting_second_line">
-              <a href="#footer" onClick={() => meetingBtn()}>
+            <p className="meeting_second_line mb-0 mb-xl-3" id="line_2">
+              <a href="#line_3"
+               onClick={() => meetingBtnLine2()}
+              >
                 <span className="custom_tab_button_012 cursor-pointer common_button color_dark_gray mx-2 font_2xl ms-sm-3 ms-lg-0 me-sm-3 meeting_custom_top_space">
                   come
                 </span>
               </a>
               usiamo il
-              <a href="#footer" onClick={() => meetingBtn()}>
+              <a href="#line_3"
+               onClick={() => meetingBtnLine2()}
+              >
                 <span className="custom_tab_button_012 cursor-pointer common_button color_dark_gray me-2 font_2xl mx-sm-3 meeting_custom_top_space">
                   marketing converazionale
                 </span>
               </a>
               e gli
-              <a href="#footer" onClick={() => meetingBtn()}>
+              <a href="#line_3"
+               onClick={() => meetingBtnLine2()}
+              >
                 <span className="custom_tab_button_012 cursor-pointer common_button color_dark_gray font_2xl mx-2 ms-lg-3 me-sm-3 meeting_custom_top_space">
                   strumenti tech<span className="ff_math">-</span>driven
                 </span>
               </a>
               da noi sviluppati
             </p>
-            {/* </div> */}
-            {/* <div className="meeting_two_block"> */}
-            <p className="meeting_third_line">
+            <p className="meeting_third_line mb-0 mb-xl-3"id="line_3">
               per ottenere una
-              <a href="#footer" onClick={() => meetingBtn()}>
+              <a href="#line_4"
+               onClick={() => meetingBtnLine3()}
+              >
                 <span className="custom_tab_button_012 cursor-pointer common_button color_dark_gray font_2xl me-2 ms-lg-3 me-sm-3 meeting_custom_top_space">
                   qualifica digitale real<span className="ff_math">-</span>
                   time
@@ -124,67 +195,84 @@ function Meeting() {
               </a>
               e trasmetterti così i dati.
             </p>
-            <p className="meeting_fourth_line">
+            <p className="meeting_fourth_line mb-0 mb-xl-3" id="line_4">
               Vogliamo utilizzare l’esperienza acquisita nel
-              <a href="#footer" onClick={() => meetingBtn()}>
+              <a href="#line_5"
+               onClick={() => meetingBtnLine4()}
+              >
                 <span className="custom_tab_button_012 cursor-pointer common_button color_dark_gray mx-2 font_2xl mx-sm-3 meeting_custom_top_space">
                   tempo
                 </span>
               </a>
               per ideare una strategia
-              <a href="#footer" onClick={() => meetingBtn()}>
+              <a href="#line_5"
+               onClick={() => meetingBtnLine4()}
+              >
                 <span className="custom_tab_button_012 cursor-pointer common_button color_dark_gray mx-2 font_2xl ms-xl-3 me-sm-3 me-xl-0 meeting_custom_top_space">
                   personalizzata
                 </span>
               </a>
             </p>
-            <p className="meeting_fifth_line">
+            <p className="meeting_fifth_line mb-0 mb-xl-3"id="line_5">
               proprio come se fossimo il tuo
-              <a href="#footer" onClick={() => meetingBtn()}>
+              <a href="#line_6"
+               onClick={() => meetingBtnLine5()}
+              >
                 <span className="custom_tab_button_012 cursor-pointer common_button color_dark_gray me-2 font_2xl mx-sm-3 meeting_custom_top_space">
                   partner
                 </span>
               </a>
               integrato nel tuo team e nel tuo
-              <a href="#footer" onClick={() => meetingBtn()}>
+              <a href="#line_6"
+               onClick={() => meetingBtnLine5()}
+              >
                 <span className="custom_tab_button_012 cursor-pointer common_button color_dark_gray mx-2 font_2xl me-sm-0 ms-sm-3 meeting_custom_top_space">
                   CRM
                 </span>
               </a>
             </p>
-            {/* </div> */}
           </div>
 
-          <div
+          {/* <div
             id="meeting-paragraph"
             className="mb-0 font_5xl color_dark_gray custom_letter_spacing meeting_para px-2 px-sm-0 mx-auto text-center d-xl-none"
           >
             Ora hai compreso che il futuro delle tue
-            <a href="#footer" onClick={() => meetingBtn()}>
+            <a href="#line_2"
+             onClick={() => meetingBtnLine1()}
+            >
               <span className="custom_tab_button_012 cursor-pointer common_button color_dark_gray mx-2 font_2xl mx-sm-3">
                 vendite
               </span>
             </a>
             è digitale e hai scoperto
-            <a href="#footer" onClick={() => meetingBtn()}>
+            <a href="#line_3"
+             onClick={() => meetingBtnLine2()}
+            >
               <span className="custom_tab_button_012 cursor-pointer common_button color_dark_gray mx-2 font_2xl ms-sm-3 ms-lg-0 me-sm-3 meeting_custom_top_space">
                 come
               </span>
             </a>
             usiamo il
-            <a href="#footer" onClick={() => meetingBtn()}>
+            <a href="#line_3"
+             onClick={() => meetingBtnLine2()}
+            >
               <span className="custom_tab_button_012 cursor-pointer common_button color_dark_gray me-2 font_2xl mx-sm-3 meeting_custom_top_space">
                 marketing converazionale
               </span>
             </a>
             e gli
-            <a href="#footer" onClick={() => meetingBtn()}>
+            <a href="#line_3"
+             onClick={() => meetingBtnLine2()}
+            >
               <span className="custom_tab_button_012 cursor-pointer common_button color_dark_gray font_2xl mx-2 ms-lg-3 me-sm-3 meeting_custom_top_space">
                 strumenti tech<span className="ff_math">-</span>driven
               </span>
             </a>
             da noi sviluppati&nbsp; per ottenere una
-            <a href="#footer" onClick={() => meetingBtn()}>
+            <a href="#line_4"
+             onClick={() => meetingBtnLine3()}
+            >
               <span className="custom_tab_button_012 cursor-pointer common_button color_dark_gray font_2xl me-2 ms-lg-3 me-sm-3 meeting_custom_top_space">
                 qualifica digitale real<span className="ff_math">-</span>time
               </span>
@@ -193,13 +281,17 @@ function Meeting() {
             e trasmetterti così i dati.
             <br />
             Vogliamo utilizzare l’esperienza acquisita nel
-            <a href="#footer" onClick={() => meetingBtn()}>
+            <a href="#line_5"
+             onClick={() => meetingBtnLine4()}
+            >
               <span className="custom_tab_button_012 cursor-pointer common_button color_dark_gray mx-2 font_2xl mx-sm-3 meeting_custom_top_space">
                 tempo
               </span>
             </a>
             per ideare una strategia
-            <a href="#footer" onClick={() => meetingBtn()}>
+            <a href="#line_5"
+             onClick={() => meetingBtnLine4()}
+            >
               <span className="custom_tab_button_012 cursor-pointer common_button color_dark_gray mx-2 font_2xl ms-xl-3 me-sm-3 me-xl-0 meeting_custom_top_space">
                 personalizzata
               </span>
@@ -209,8 +301,8 @@ function Meeting() {
             se fossimo il tuo
             <a
               className="ms-2 ms-sm-0"
-              href="#footer"
-              onClick={() => meetingBtn()}
+              href="#line_6"
+              onClick={() => meetingBtnLine5()}
             >
               <span className="custom_tab_button_012 cursor-pointer common_button color_dark_gray me-2 font_2xl mx-sm-3 meeting_custom_top_space">
                 partner
@@ -218,15 +310,19 @@ function Meeting() {
             </a>
             <div className="d-none d-sm-block">
               integrato nel tuo team e nel tuo
-              <a href="#footer" onClick={() => meetingBtn()}>
+              <a href="#line_6"
+                onClick={() => meetingBtnLine5()}
+              >
                 <span className="custom_tab_button_012 cursor-pointer common_button color_dark_gray mx-2 font_2xl me-sm-0 ms-sm-3 meeting_custom_top_space">
                   CRM
                 </span>
               </a>
             </div>
-          </div>
+          </div> */}
         </div>
-        <Footer />
+        <div id="line_6">
+          <Footer />
+          </div>
       </div>
     </>
   );
